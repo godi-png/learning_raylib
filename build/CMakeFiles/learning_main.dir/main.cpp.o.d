530d3732396ace26daea070a88ddee68b98e9a2b
godi-png <eraticsyndicate@gmail.com>
2025-07-09 14:49:39 -0600

    initial commit

---
+++ build/CMakeFiles/learning_main.dir/main.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/learning_main.dir/main.cpp.o: \
+ /home/godine/Projects/raylib/learning/main.cpp \
+ /usr/include/stdc-predef.h
---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <iostream>
+#include "raylib.h"
+
+int main(void){
+	std::cout << "Hello World!" << std::endl;
+
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+	const int target_fps = 60;
+
+	InitWindow(screenWidth, screenHeight, "pene");
+
+	SetTargetFPS(target_fps);
+
+	while(!WindowShouldClose()){
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("My First Window", 190, 200, 20, LIGHTGRAY);
+	EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
+}
